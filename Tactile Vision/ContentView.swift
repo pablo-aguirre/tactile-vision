faaac117e04956ed27737214afcd088b776e134f
Pablo Aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-08-05 17:44:13 +0200

    Initial Commit

---
+++ Tactile Vision/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tactile Vision
+//
+//  Created by Pablo Aguirre on 05/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tactile Vision/Tactile_VisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tactile_VisionApp.swift
+//  Tactile Vision
+//
+//  Created by Pablo Aguirre on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Tactile_VisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
